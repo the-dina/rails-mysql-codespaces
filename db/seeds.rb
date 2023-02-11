71855288ae934fbcb24f9f82a094497f03c3b7cd
Dina Esmaeili <120369808+the-dina@users.noreply.github.com>
2023-02-11 09:41:58 +0000

    List of books from the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Book.create!([{
+    title: "Atomic Habits",
+    author: "James Clear",
+    description: "the life-changing million-copy #1 bestselle",
+  },
+  {
+    title: "Essentialism",
+    author: "Greg McKeown",
+    description: "The Disciplined Pursuit of Less",
+  },
+  {
+   title: "Listening Skills Training",
+    author: "James W Williams",
+    description: "How to Truly Listen, Understand, and Validate for Better and Deeper Connections",
+  }])
