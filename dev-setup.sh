a62dea118169b8b859b291b9bb21024091791645
Dina Esmaeili <120369808+the-dina@users.noreply.github.com>
2023-02-11 08:50:56 +0000

    dev container setup

---
+++ dev-setup.sh
@@ -0,0 +1,10 @@
+apt-get update
+export DEBIAN_FRONTEND=noninteractive
+apt-get install -y mariadb-client
+apt-get install -y libmysqlclient-dev
+apt-get clean -y && rm -rf /var/lib/apt/lists/*
+
+# bundle install
+# rails db:create
+# rails db:migrate
+# rails db:seed
